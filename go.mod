f1962ca81cd8f3affd96c94f9901ba20cd30c53d
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-08-30 16:02:23 +0200

    Initial setup of proxy

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/jaytaph/xdbgprxy
+
+go 1.17
+
+require (
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
+	github.com/spf13/pflag v1.0.5
+	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b
+)
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/fatih/color"
+	flag "github.com/spf13/pflag"
+)
+
+const (
+	version = "v0.0.1"
+)
+
+type Config struct {
+	ideHost    string // TCP host of the IDE
+	idePort    int    // TCP port of the IDE
+	listenHost string // TCP host to listen on (0.0.0.0 for all interfaces)
+	listenPort int    // TCP port to listen on
+	verbose    bool   // true when need to display more info
+	nocolor    bool   // true when no colors are needed
+}
+
+var config Config
+
+func main() {
+	parseFlags()
+
+	color.NoColor = config.nocolor
+
+	displayLogo()
+
+	address := fmt.Sprintf("%s:%d", config.ideHost, config.idePort)
+	color.Set(color.FgHiGreen)
+	fmt.Printf("+ Checking if connection to the IDE can be made at %s\n", address)
+	ideConn, err := net.Dial("tcp", address)
+	if err != nil {
+		color.Set(color.FgHiRed)
+		fmt.Printf("! IDE does not seem to be avaiable: %s\n", err.Error())
+		fmt.Printf("  Check if you are using the correct host and port, and that the debugger in your IDE is turned on.\n")
+		os.Exit(1)
+	}
+	_ = ideConn.Close()
+
+	address = fmt.Sprintf("%s:%d", config.listenHost, config.listenPort)
+	color.Set(color.FgHiGreen)
+	fmt.Printf("+ Checking if listening port is available at %s\n", address)
+
+	// Open listening connection
+	phpServer, err := net.Listen("tcp", address)
+	if err != nil {
+		color.Set(color.FgHiRed)
+		fmt.Printf("! Port doesnt seem to be available: %s\n", err.Error())
+		os.Exit(1)
+	}
+	defer func() {
+		_ = phpServer.Close()
+	}()
+
+	color.Set(color.FgHiGreen)
+	fmt.Printf("? Set 'xdebug.client_host=%s' and 'xdebug.client_port=%d'\n", config.listenHost, config.listenPort)
+	fmt.Printf("  in your php.ini configuration to connect to this proxy.\n")
+
+
+
+	color.Set(color.FgHiGreen)
+	fmt.Println("+ Waiting for incoming PHP connections")
+
+	// Accept connections from PHP, and handle proxy between PHP and IDE
+	for {
+		phpConn, err := phpServer.Accept()
+		if err != nil {
+			log.Panic(err.Error())
+		}
+		defer func() {
+			_ = phpConn.Close()
+		}()
+
+		color.Set(color.FgHiGreen)
+		fmt.Println("- Received incoming connection from PHP")
+
+		color.Set(color.FgHiGreen)
+		fmt.Println("- Opening proxy connection to the IDE")
+		address = fmt.Sprintf("%s:%d", config.ideHost, config.idePort)
+		ideConn, err := net.Dial("tcp", address)
+		if err != nil {
+			log.Panic(err.Error())
+		}
+		defer func() {
+			_ = ideConn.Close()
+		}()
+
+		handleProxy(phpConn, ideConn)
+
+		fmt.Println("- Connection completed")
+	}
+}
+
+func handleProxy(phpConn net.Conn, ideConn net.Conn) {
+	phpChan := chanFromConn(phpConn)
+	ideChan := chanFromConn(ideConn)
+
+	defer ideConn.Close()
+	defer phpConn.Close()
+
+	for {
+		select {
+		case b1 := <-phpChan:
+			if b1 == nil {
+				color.Set(color.FgHiGreen)
+				fmt.Println("! PHP connection closed")
+				return
+			} else {
+				fmt.Println("> php: ", color.HiBlueString(string(b1)))
+				_, err := ideConn.Write(b1)
+				if err != nil {
+					color.Set(color.FgHiRed)
+					fmt.Println("! Error writing to PHP: ", err)
+				}
+			}
+		case b2 := <-ideChan:
+			if b2 == nil {
+				color.Set(color.FgHiGreen)
+				fmt.Println("! IDE connection closed")
+				return
+			} else {
+				fmt.Println("> php: ", color.HiRedString(string(b2)))
+				_, err := phpConn.Write(b2)
+				if err != nil {
+					color.Set(color.FgHiRed)
+					fmt.Println("! Error writing to IDE: ", err)
+				}
+			}
+		}
+	}
+}
+
+func displayLogo() {
+	logo := "         _ _\n" +
+		"        | | |\n" +
+		"__  ____| | |__   __ _ _ __  _ ____  ___   _\n" +
+		"\\ \\/ / _` | '_ \\ / _` | '_ \\| '__\\ \\/ / | | |\n" +
+		" >  < (_| | |_) | (_| | |_) | |   >  <| |_| |\n" +
+		"/_/\\_\\__,_|_.__/ \\__, | .__/|_|  /_/\\_\\\\__, |\n" +
+		"                  __/ | |               __/ |\n" +
+		"                 |___/|_|              |___/\n"
+
+	color.Set(color.FgHiCyan)
+	fmt.Println(logo)
+
+	color.Set(color.FgHiGreen)
+	fmt.Println("+ Starting XdbgPrxy " + version + " - Joshua Thijssen (https://github.com/jaytaph)")
+}
+
+func parseFlags() {
+	flag.StringVar(&config.ideHost, "ide-host", "127.0.0.1", "IP to your IDE")
+	flag.IntVar(&config.idePort, "ide-port", 9003, "Port to your IDE")
+	flag.StringVar(&config.listenHost, "listen-host", "127.0.0.1", "IP to listen on")
+	flag.IntVar(&config.listenPort, "listen-port", 9003, "Port to listen on")
+	flag.BoolVar(&config.verbose, "verbose", true, "Verbose output")
+	flag.BoolVar(&config.nocolor, "no-color", false, "No ANSI color output")
+
+	flag.Parse()
+}
+
+func chanFromConn(conn net.Conn) chan []byte {
+	c := make(chan []byte)
+
+	go func() {
+		b := make([]byte, 1024)
+
+		for {
+			n, err := conn.Read(b)
+			if n > 0 {
+				res := make([]byte, n)
+				copy(res, b[:n])
+				c <- res
+			}
+			if err != nil {
+				c <- nil
+				break
+			}
+		}
+	}()
+
+	return c
+}
